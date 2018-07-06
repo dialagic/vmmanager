6c0d13cef971598369da89e3ee11bbba2926711f
dialagic <40882096+dialagic@users.noreply.github.com>
2018-07-06 13:01:00 +0200

    Update vminfo.sh

--- vminfo.sh
+++ vminfo.sh
@@ -25,8 +25,7 @@ function response() {
 function get_load() {
 
 	load_curr_1=$(cat /proc/loadavg | awk -F" " "{print \$1}")
-	load_curr_5=$(cat /proc/loadavg | awk -F" " "{print \$2}") # Ovo zakomentirat ako se fejka
-	#load_curr_5=7.00 # Ovdje harkodirat
+	load_curr_5=$(cat /proc/loadavg | awk -F" " "{print \$2}")
 	load_curr_15=$(cat /proc/loadavg | awk -F" " "{print \$3}")
 	
 	LOAD=$(echo "load_1=${load_curr_1}\nload_5=${load_curr_5}\nload_15=${load_curr_15}")
@@ -39,8 +38,7 @@ function get_ram_usage() {
 	free=$(free | awk '/Mem/ {print $4}')
 	available=$(free | awk '/buffers\/cache/ {print $4}')
 	free_percentage=$(echo "scale=2; ${free}*100/${total}" | bc)
-	available_percentage=$(echo "scale=2; ${available}*100/${total}" | bc) # Ovo zakomentirat ako se fejka
-	#available_percentage='5.6' # Ovdje harkodirat
+	available_percentage=$(echo "scale=2; ${available}*100/${total}" | bc)
 	
 	RAM="\ntotal=${total}\nfree=${free}\navailable=${available}\nfree_percentage=${free_percentage}\navailable_percentage=${available_percentage}"
 
